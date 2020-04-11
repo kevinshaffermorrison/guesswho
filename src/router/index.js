dd55143f2ee89e59a69e80872d5834b01d8764f7
Kevin Shaffer-Morrison <kevinshaffera@gmail.com>
2020-04-11 18:18:10 -0400

    Splitting out some logic and making the app more concurrency safe

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Room from '../views/Room.vue'
-import Test from '../views/Test.vue'
-import Room2 from '../views/Room2.vue'
 
 Vue.use(VueRouter)
 
@@ -12,16 +10,6 @@ const routes = [
         path: '/rooms/:room',
         name: 'Room',
         component: Room
-    },
-    {
-        path: '/rooms2/:room',
-        name: 'Room2',
-        component: Room2
-    },
-    {
-        path: '/rooms/',
-        name: 'Test',
-        component: Test
     },
     {
         path: '/',
