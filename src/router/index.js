54caefddc991c72ffc7f8c8eec5ae448ab5b3d2b
Kevin Shaffer-Morrison <kevinshaffera@gmail.com>
2020-04-11 15:16:37 -0400

    'Transactions'

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Room from '../views/Room.vue'
+import Test from '../views/Test.vue'
+import Room2 from '../views/Room2.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +12,16 @@ const routes = [
         path: '/rooms/:room',
         name: 'Room',
         component: Room
+    },
+    {
+        path: '/rooms2/:room',
+        name: 'Room2',
+        component: Room2
+    },
+    {
+        path: '/rooms/',
+        name: 'Test',
+        component: Test
     },
     {
         path: '/',
