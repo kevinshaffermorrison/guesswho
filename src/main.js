551c097c9683ceadd79ed16ad05ebd57e4e78a2d
Kevin Shaffer-Morrison <kevinshaffera@gmail.com>
2020-04-12 21:29:29 -0400

    more descript icons

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapVue from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faDoorOpen, faUser, faPlayCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUserSecret)
+library.add(faUserSecret);
+library.add(faDoorOpen);
+library.add(faUser);
+library.add(faPlayCircle);
+
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
